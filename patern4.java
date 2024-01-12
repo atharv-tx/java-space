1d5ec6aa3ced2c9638a78f132aedde5480c03d5c
atharv-tx <124334428+atharv-tx@users.noreply.github.com>
2024-01-12 13:54:38 +0530

    pattern

---
+++ patern4.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class patern4 {
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+
+        
+        int st =1 ;
+        int sp = 2*n-2;
+
+        for (int number_of_line=1; number_of_line <= 2*n; number_of_line++){
+
+            // print stars 
+            for(int i=0;i<st; i++){
+                System.out.print("* ");
+            }
+
+            // print spaces 
+            for(int i=0;i<sp; i++){
+                System.out.print("  ");
+            }
+
+            //print stars 
+
+            for(int i=0;i<st; i++){
+                System.out.print("* ");
+            }
+            // nxt line 
+            if(number_of_line<n){
+                st++;
+                sp=sp-2;
+            }else if(number_of_line>n) {
+                st--;
+                sp=sp+2;
+            }
+            System.out.println();
+            
+
+        }
+       
+        
+    }
+    
+}
+    
+
